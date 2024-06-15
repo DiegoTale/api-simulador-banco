179e6330f6c703a5c2f92961159e152d21f001cd
Luis Castillo <luis.castillo@soytamarindo.com>
2024-06-14 19:08:10 -0600

    se agrega type-orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 
 import { UsersModule } from './app/users/users.module';
 import { ClientsModule } from './app/clients/clients.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, ClientsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'simulador-banco.cluster-c7c2km4au4qf.us-east-2.rds.amazonaws.com',
+      port: 3306,
+      username: 'admin',
+      password: 'kzW3nVchgozgTdav',
+      database: 'simulador-banco',
+      entities: [],
+      synchronize: true,
+    }),
+    UsersModule,
+    ClientsModule],
   controllers: [],
   providers: [],
 })
